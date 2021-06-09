8a80e4bd57db931d7e8726829c7c975f96608208
Will Lane <williamlane923@gmail.com>
2021-06-08 17:42:15 -0700

    rustify

---
+++ rustify.cpp
@@ -0,0 +1,12 @@
+#include "rustify.hpp"
+#include <iostream>
+#include <string.h>
+
+fn hello(String h) -> i32 {
+  println("Hello World");
+}
+
+fn main() -> i32 {
+  println("Hello World");
+  return 0;
+}
---
+++ rustify.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <string.h>
+#include <iostream>
+
+#define use #include
+#define i32 int
+#define i64 __int64
+#define fn auto
+#define str *char
+#define String std::string
+#define println(x) std::cout << x << std::endl;
