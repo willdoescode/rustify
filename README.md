28ec408ddcc857a8825943c11d7951b5c594baa2
Will Lane <williamlane923@gmail.com>
2021-06-08 17:58:53 -0700

    rustify

--- README.md
+++ README.md
@@ -6,13 +6,26 @@
 #include "rustify.hpp"
 #include <iostream>
 #include <string.h>
+#include <vector>
 
 fn hello(String h) -> i32 {
   println("Hello World");
+  return 5;
+}
+
+fn other(str x) -> void {
+  println(x);
 }
 
 fn main() -> i32 {
   println("Hello World");
+  other("Hello Other");
+
+  Vec<i32> x = {1, 2, 3, 4};
+
+  for (auto& i : x)
+    println(i);
+
   return 0;
 }
 ```
--- rustify.hpp
+++ rustify.hpp
@@ -2,11 +2,20 @@
 
 #include <string.h>
 #include <iostream>
+#include <vector>
 
 #define use #include
+
+#define i16 __int16
 #define i32 int
 #define i64 __int64
+#define i128 __int128
+
 #define fn auto
-#define str *char
+#define str const char*
 #define String std::string
 #define println(x) std::cout << x << std::endl;
+#define struct class
+#define trait concept
+
+#define Vec std::vector
